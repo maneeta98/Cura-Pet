72e1cee3434d008a926b0f810f0595b5da87dab7
maneeta98 <manitapariyar2005@gmail.com>
2025-05-19 15:27:06 +0545

    Login screen design

---
+++ lib/view/login_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Login')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: [
+              Image.asset('assets/CURA PET.png', height: 100),
+              const SizedBox(height: 20),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              // ElevatedButton(
+              //   onPressed: () {
+              //     Navigator.pushReplacementNamed(context, '/dashboard');
+              //   },
+              //   child: const Text('Login'),
+              // ),
+              // TextButton(
+              //   onPressed: () => Navigator.pushNamed(context, '/register'),
+              //   child: const Text('Do not have an account? Register'),
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
